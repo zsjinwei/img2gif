942412f6ddf5722b2e8d01e00da5b5fee6a63a87
黄锦伟 <huangjinwei@meizu.com>
2018-11-22 14:59:40 +0800

    进行模板匹配
    Change-Id: I9d8673142cc6623ed71651a79bb8b91fc5194b7e

--- img2gif.py
+++ img2gif.py
@@ -6,6 +6,8 @@ Created on 2018-11-22
 '''
 import imageio
 import os
+import cv2 as cv
+import numpy as np
 
 def getImgFileList(file_dir):
     L=[]
@@ -15,15 +17,61 @@ def getImgFileList(file_dir):
                    L.append(os.path.join(root, file))
     return L
 
+def mergeFramesToGif(frames):
+    pass
+
 if __name__ == '__main__':
     outGifFileName = "./out/out.gif"
     imgFiles = getImgFileList("./images")
-    print(imgFiles)
+    # print(imgFiles)
 
-    frames = []  
+    #frames = []  
+    #for image_name in imgFiles:  
+    #    frames.append(imageio.imread(image_name))
+
+    # 提取模板
+    # 定位模板位置百分比参数
+    sXp = 0.29  # 左上角X坐标位置百分比
+    sYp = 0.27  # 左上角Y坐标位置百分比
+    Wp = 0.07   # 宽度百分比
+    Hp = 0.1    # 高度百分比
+    tplSource = cv.imread("./images/1.jpg")
+    tplSize = tplSource.shape
+    sX = int(tplSize[0] * sXp)
+    sY = int(tplSize[1] * sYp)
+    tX = int(sX + tplSize[0] * Wp)
+    tY = int(sY + tplSize[1] * Hp)
+
+    # 显示模板框
+    #cv.namedWindow('template image', cv.WINDOW_NORMAL)
+    #cv.rectangle(tplSource, (sX, sY), (tX, tY), (0, 0, 255), 2)
+    #cv.imshow("template image", tplSource)
+
+    tpl = tplSource[sY:tY,sX:tX]
+    cv.imwrite('./out/tpl.jpg', tpl)
+    
+    methods = [cv.TM_SQDIFF_NORMED, cv.TM_CCORR_NORMED, cv.TM_CCOEFF_NORMED]   #3种模板匹配方法
+    th, tw = tpl.shape[:2]
+
     for image_name in imgFiles:  
-        frames.append(imageio.imread(image_name))
+        target = cv.imread(image_name)
 
+        md = methods[2] # 选择匹配方法
+
+        result = cv.matchTemplate(target, tpl, md)
+
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+        if md == cv.TM_SQDIFF_NORMED:
+            tl = min_loc
+        else:
+            tl = max_loc
+        br = (tl[0]+tw, tl[1]+th)   #br是矩形右下角的点的坐标
+        cv.rectangle(target, tl, br, (0, 0, 255), 2)
+        cv.namedWindow("match-" + image_name, cv.WINDOW_NORMAL)
+        cv.imshow("match-" + image_name, target)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
     # Save them as frames into a gif   
-    imageio.mimsave(outGifFileName, frames, 'GIF', duration = 0.8) 
+    # imageio.mimsave(outGifFileName, frames, 'GIF', duration = 0.8) 
 
