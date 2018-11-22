0ddad3502a5657c3f2be8d871ceb9e17e1763486
黄锦伟 <huangjinwei@meizu.com>
2018-11-22 19:41:48 +0800

    增加第二特征模板匹配
    Change-Id: I0d3051412456ef47788759ea314233e06cb879c0

--- img2gif.py
+++ img2gif.py
@@ -17,22 +17,8 @@ def getImgFileList(file_dir):
                    L.append(os.path.join(root, file))
     return L
 
-def mergeFramesToGif(frames):
-    pass
-
-if __name__ == '__main__':
-    outGifFileName = "./out/out.gif"
-    imgFiles = getImgFileList("./images")
-    imgFiles.sort()
-    # print(imgFiles)
-
-    # 提取模板
-    # 定位模板位置百分比参数
-    sXp = 0.29  # 左上角X坐标位置百分比
-    sYp = 0.27  # 左上角Y坐标位置百分比
-    Wp = 0.07   # 宽度百分比
-    Hp = 0.1    # 高度百分比
-    tplSource = cv.imread("./images/1.jpg")
+def getTpl(imgPath, sXp, sYp, Wp, Hp):
+    tplSource = cv.imread(imgPath)
     tplSize = tplSource.shape
     sX = int(tplSize[0] * sXp)
     sY = int(tplSize[1] * sYp)
@@ -42,14 +28,55 @@ if __name__ == '__main__':
     # 显示模板框
     #cv.namedWindow('template image', cv.WINDOW_NORMAL)
     #cv.rectangle(tplSource, (sX, sY), (tX, tY), (0, 0, 255), 2)
-    #cv.imshow("template image", tplSource)
+    #cv.imshow(imgPath, tplSource)
 
     tpl = tplSource[sY:tY,sX:tX]
-    cv.imwrite('./out/tpl.jpg', tpl)
-    
+    return tpl
+
+def matchTpl(target, tpl):
     methods = [cv.TM_SQDIFF_NORMED, cv.TM_CCORR_NORMED, cv.TM_CCOEFF_NORMED]   #3种模板匹配方法
+    md = methods[2] # 选择匹配方法
+
     th, tw = tpl.shape[:2]
 
+    result = cv.matchTemplate(target, tpl, md)
+
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    if md == cv.TM_SQDIFF_NORMED:
+        tl = min_loc
+    else:
+        tl = max_loc
+    
+    br = (tl[0]+tw, tl[1]+th)   #br是矩形右下角的点的坐标
+    
+    # 记录图像宽高和匹配坐标中心点
+    cpX = int(tl[0]+tw/2)
+    cpY = int(tl[1]+th/2)
+
+    return cpX, cpY, tl, br
+
+if __name__ == '__main__':
+    outGifFileName = "./out/out.gif"
+    imgFiles = getImgFileList("./images")
+    imgFiles.sort()
+    # print(imgFiles)
+
+    # 提取模板
+    # 定位模板位置百分比参数
+    sXp = 0.25  # 左上角X坐标位置百分比
+    sYp = 0.06  # 左上角Y坐标位置百分比
+    Wp = 0.11   # 宽度百分比
+    Hp = 0.1    # 高度百分比
+    tpl = getTpl("./images/6.jpg", sXp, sYp, Wp, Hp)
+
+    cv.imwrite('./out/tpl.jpg', tpl)
+
+    # 提取另一个模板, 用于计算缩放/旋转系数
+    tpl2 = getTpl("./images/6.jpg", 0.285, 0.49, 0.040, 0.062)
+    #tpl2 = getTpl("./images/6.jpg", 0.70, 0.68, 0.045, 0.08)
+
+    cv.imwrite('./out/tp2.jpg', tpl2)
+    #exit()
     cpMap = {}
 
     # 匹配模板并获取模板匹配中心坐标
@@ -57,25 +84,15 @@ if __name__ == '__main__':
         target = cv.imread(imageName)
         targetH, targetW = target.shape[:2]
 
-        md = methods[2] # 选择匹配方法
+        cpX, cpY, tl, br = matchTpl(target, tpl)
+        cpX2, cpY2, tl2, br2 = matchTpl(target, tpl2)
 
-        result = cv.matchTemplate(target, tpl, md)
+        cpMap[imageName] = [targetW, targetH, cpX, cpY, cpX2, cpY2]
 
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-        if md == cv.TM_SQDIFF_NORMED:
-            tl = min_loc
-        else:
-            tl = max_loc
-        br = (tl[0]+tw, tl[1]+th)   #br是矩形右下角的点的坐标
-
-        # 记录图像宽高和匹配坐标中心点
-        cpX = int(tl[0]+tw/2)
-        cpY = int(tl[1]+th/2)
-        cpMap[imageName] = [targetW, targetH, cpX, cpY]
-        #cv.rectangle(target, tl, br, (0, 0, 255), 2)
-        #cv.namedWindow("match-" + imageName, cv.WINDOW_NORMAL)
-        #cv.imshow("match-" + imageName, target)
-
+        cv.rectangle(target, tl, br, (0, 0, 255), 4)
+        cv.rectangle(target, tl2, br2, (0, 255, 255), 4)
+        cv.namedWindow("match-" + imageName, cv.WINDOW_NORMAL)
+        cv.imshow("match-" + imageName, target)
     #print(cpMap)
 
     minTopSize = float("inf")
@@ -108,14 +125,14 @@ if __name__ == '__main__':
         stripImg = currentImg[int(item[3] - minTopSize):int(item[3] + minBottomSize), int(item[2] - minLeftSize):int(item[2] + minRightSize)]
         frames.append(stripImg)
 
-        tt = cv.imread(img)
-        cv.rectangle(tt, (int(item[2] - minLeftSize), int(item[3] - minTopSize)), (int(item[2] + minRightSize), int(item[3] + minBottomSize)), (0, 0, 255), 2)
-        cv.namedWindow("match-" + img, cv.WINDOW_NORMAL)
-        cv.imshow("match-" + img, tt)
+        #tt = cv.imread(img)
+        #cv.rectangle(tt, (int(item[2] - minLeftSize), int(item[3] - minTopSize)), (int(item[2] + minRightSize), int(item[3] + minBottomSize)), (0, 0, 255), 2)
+        #cv.namedWindow("match-" + img, cv.WINDOW_NORMAL)
+        #cv.imshow("match-" + img, tt)
 
     # Save them as frames into a gif   
     imageio.mimsave(outGifFileName, frames, 'GIF', duration = 0.4)
 
     #cv.waitKey(0)
-    #cv.destroyAllWindows()
+    cv.destroyAllWindows()
 
